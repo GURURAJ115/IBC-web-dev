56db07394b940bed62c9f59c7ad2e60e2f3b7fda
Gururaj <gururajsnaik115@gmail.com>
2023-08-10 23:03:16 +0530

    Added different pages and components and routed it

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,16 +5,46 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/partials/header/header.component';
 import { HomeComponent } from './components/pages/home/home.component';
+import { FooterComponent } from './components/partials/footer/footer.component';
+import { AboutusComponent } from './components/pages/aboutus/aboutus.component';
+import { PagesComponent } from './components/pages/pages.component';
+import { TempleComponent } from './components/pages/temple/temple.component';
+import { DonationsComponent } from './components/pages/donations/donations.component';
+import { SpComponent } from './components/pages/sp/sp.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent,
+    AboutusComponent,
+    PagesComponent,
+    TempleComponent,
+    DonationsComponent,
+    SpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path:'aboutus',
+        component:AboutusComponent
+      },
+      {
+        path: 'sp',
+        component:SpComponent },
+      {
+        path:'temple',
+        component:TempleComponent
+      },
+      {
+        path:'donations',
+        component:DonationsComponent
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/components/pages/temple/temple.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-temple',
+  templateUrl: './temple.component.html',
+  styleUrls: ['./temple.component.css']
+})
+export class TempleComponent {
+
+}
